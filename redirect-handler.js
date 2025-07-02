1fae70b74f86df0b97daa4f461710f356597e135
Krivodushie <151623916+Krivodushie@users.noreply.github.com>
2025-07-03 01:45:44 +0300

    Delete redirect-handler.js

--- redirect-handler.js
+++
@@ -1,5 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const redirectPath = urlParams.get('redirect');
-if (redirectPath) {
-  window.history.replaceState(null, '', redirectPath);
-}
